e43cd795100d9c8d7e1b9d7c0b4f94157e0a452c
Sean.Malone <sean.malone@insight.com>
2024-04-17 16:22:48 -0400

    first commit

---
+++ Api/Hubs/ChatHub.cs
@@ -0,0 +1,32 @@
+﻿using Api.Services;
+using Microsoft.AspNetCore.SignalR;
+
+namespace Api.Hubs
+{
+	public class ChatHub: Hub
+	{
+		private readonly ChatService _chatService;
+
+		public ChatHub(ChatService chatService)
+		{
+			_chatService = chatService;
+        }
+
+		public override async Task OnConnectedAsync()
+		{
+			await Groups.AddToGroupAsync(Context.ConnectionId, "Come2Chat");
+			await Clients.Caller.SendAsync("UserConnected");
+		}
+		public override async Task OnDisconnectedAsync(Exception exception)
+		{
+			await Groups.RemoveFromGroupAsync(Context.ConnectionId, "Come2Chat");
+			await base.OnDisconnectedAsync(exception);
+		}
+		public async Task AddUserConnectionId(string name) { _chatService.AddUsersConnectionId(name, Context.ConnectionId);
+			var onlineUsers = _chatService.GetOnlineUsers();
+			await Clients.Groups("Come2Chat").SendAsync("OnlineUsers", onlineUsers);
+		}
+
+    }
+}
+
--- Api/Program.cs
+++ Api/Program.cs
@@ -1,26 +1,30 @@
-﻿var builder = WebApplication.CreateBuilder(args);
+﻿using Api.Hubs;
+using Api.Services;
+//using Microsoft.AspNetCore.Builder;
+//using Microsoft.Extensions.DependencyInjection;
+//using Microsoft.Extensions.Hosting;
+
+var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 
 builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
-builder.Services.AddSwaggerGen();
+
+builder.Services.AddSingleton<ChatService>();
+builder.Services.AddSignalR();
+builder.Services.AddCors();
 
 var app = builder.Build();
 
-// Configure the HTTP request pipeline.
-if (app.Environment.IsDevelopment())
-{
-    app.UseSwagger();
-    app.UseSwaggerUI();
-}
+app.UseCors(x => x.AllowAnyHeader().AllowAnyMethod().AllowCredentials().WithOrigins("http://localhost:4200"));
 
 app.UseHttpsRedirection();
 
 app.UseAuthorization();
 
 app.MapControllers();
-
+app.MapHub<ChatHub>("/hubs/chat");
 app.Run();
 
